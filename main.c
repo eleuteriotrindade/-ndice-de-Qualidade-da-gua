54be6c47663b568396dfa1acf78675c0dc821a0a
AlucardFAS <FernandoImp@outlook.com.br>
2016-06-07 19:06:53 -0300

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,15 @@
 #define e 2.718281828459045235360287
 #define pi 3.14159265358979323846
 
+//Oxigenio dissolvido(OD), Coliformes Fecais(CF), Potencial Hidrogenionico(pH), Demanda Bioquimica de Oxigenio(DBO), Nitratos(NO), Fosfatos(PO), Temperatura(C), Turbidez(UNT), Residuos Totais(RT)
+//OD CF pH DBO NO PO C UNT RT
+double peso[9]={0.17,0.15,0.12,0.10,0.10,0.10,0.10,0.08,0.08} 
+double OD()
+{
+
+
+}
+
 int main(){
 
 
